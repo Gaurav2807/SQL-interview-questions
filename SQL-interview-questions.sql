7685e5f8b29522e096c01ca6c642902d72a635cd
Gaurav <gaurav2899gupta@gmail.com>
2025-09-03 16:03:31 +0530

    2. Top 2 employees with highest salary within each department

--- SQL-interview-questions.sql
+++ SQL-interview-questions.sql
@@ -8,3 +8,16 @@ Select
 	limit 2;
 
 	
+-- 2. Top 2 employees with highest salary within each department
+With CTE_department_id_as_window as 
+(
+	Select 
+		*, 
+		row_number() over(partition by department_id order by salary desc) row_num
+		from Employee
+)
+Select 
+	department_id, emp_id, emp_name, salary
+	from CTE_department_id_as_window
+	where row_num <= 2;
+

f47a0ab49d854d4ffe5a2d544355413bdfea5269
Gaurav <gaurav2899gupta@gmail.com>
2025-09-03 16:02:42 +0530

    1. Top 2 employees with highest salary

--- SQL-interview-questions.sql
+++ SQL-interview-questions.sql
@@ -1,1 +1,10 @@
 ------------------------------------------- Problem statements and their solutions ---------------------------------------------------
+
+-- 1. Top 2 employees with highest salary
+Select 
+	* 
+	from Employee
+	order by salary desc
+	limit 2;
+
+	
